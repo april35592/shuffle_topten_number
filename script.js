eac5974a576d86c6083de26129d86843d1b9c141
사월 <77724156+april35592@users.noreply.github.com>
2023-05-02 19:11:06 +0900

    first upload

---
+++ script.js
@@ -0,0 +1,82 @@
+openHtml();
+
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+let order = -1;
+let shuffled = false;
+
+function openHtml() {
+  const shuffleBtn = document.querySelector("#shuffleBtn");
+  const remindInput = document.querySelector("#remindInput");
+  const remindBtn = document.querySelector("#remindBtn");
+  const card = document.querySelector(".card");
+  const orderBtnGroup = document.querySelector(".orderBtnGroup");
+  const orderBtns = document.getElementsByClassName("orderBtn");
+  const selectOrder = document.querySelector(".selectOrder");
+
+  shuffleBtn.addEventListener("click", clickShuffle);
+  remindBtn.addEventListener("click", clickRemind);
+  for (let i = 0; i < orderBtns.length; i++) {
+    orderBtns.item(i).addEventListener("click", clickOrder);
+  }
+
+  function clickShuffle(event) {
+    event.preventDefault();
+    numbers = arrayShuffle(numbers);
+    remindInput.value = encrypt(numbers);
+    shuffled = true;
+    cardWrite();
+  }
+
+  function clickRemind(event) {
+    event.preventDefault();
+    try {
+      numbers = decrypt(remindInput.value);
+      shuffled = true;
+      cardWrite();
+    } catch {
+      remindInput.value = "잘못된 코드입니다.";
+    }
+  }
+
+  function clickOrder(event) {
+    selectOrder.textContent = this.textContent;
+    order = Number(this.value) - 1;
+    hideElement(orderBtnGroup);
+    displayElement(selectOrder);
+    if (shuffled) {
+      cardWrite();
+    }
+  }
+
+  function cardWrite() {
+    if (order != -1) {
+      card.textContent = numbers[order];
+    }
+  }
+}
+
+function encrypt(arr) {
+  return CryptoJS.AES.encrypt(JSON.stringify(arr), "").toString();
+}
+
+function decrypt(string) {
+  return JSON.parse(CryptoJS.AES.decrypt(string, "").toString(CryptoJS.enc.Utf8));
+}
+
+function arrayShuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomPosition = Math.floor(Math.random() * (i + 1));
+    const temporary = array[i];
+    array[i] = array[randomPosition];
+    array[randomPosition] = temporary;
+  }
+  return array;
+}
+
+function hideElement(el) {
+  el.classList.add("hide");
+}
+
+function displayElement(el) {
+  el.classList.remove("hide");
+}
