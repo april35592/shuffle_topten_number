12a8b473e4b7fdc0b736dfbab47dc563ad98b440
사월 <77724156+april35592@users.noreply.github.com>
2023-05-04 17:21:38 +0900

    script aditing

--- script.js
+++ script.js
@@ -1,52 +1,143 @@
-openHtml();
+openHTML();
 
-function openHtml() {
-  let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-  let maxNumber = 3;
-  let yourOrder = -1;
+function openHTML() {
+  header();
+  footer();
+  pageCreateOrJoin();
+}
 
-  howButton();
-  setNumber();
-  tabCreateOrJoin();
+function header() {
+  titleBtn();
+  howBtn();
+}
 
-  function howButton() {
-    document.querySelector(".howBtn").addEventListener("click", () => {
-      removeClassToElement(document.querySelector(".howTo"));
-    });
-    document.querySelector(".howCloseBtn").addEventListener("click", () => {
+function titleBtn() {
+  document.querySelector("h1").addEventListener("click", () => {
+    location.reload();
+  });
+}
+
+function howBtn() {
+  document.querySelector(".howBtn").addEventListener("click", () => {
+    removeClassToElement(document.querySelector(".howTo"));
+  });
+  document.querySelector(".howCloseBtn").addEventListener("click", () => {
+    addClassToElement(document.querySelector(".howTo"));
+  });
+  document.querySelector(".howTo").addEventListener("click", e => {
+    if (e.target === document.querySelector(".howTo")) {
       addClassToElement(document.querySelector(".howTo"));
+    }
+  });
+}
+
+function pageCreateOrJoin() {
+  tabCreateOrJoin();
+  createGame();
+  joinGame();
+}
+
+function tabCreateOrJoin() {
+  document.querySelector(".createTabBtn").addEventListener("click", () => {
+    removeClassToElement(document.querySelector(".create"));
+    addClassToElement(document.querySelector(".join"));
+    addClassToElement(document.querySelector(".createTabBtn"), "select");
+    removeClassToElement(document.querySelector(".joinTabBtn"), "select");
+  });
+  document.querySelector(".joinTabBtn").addEventListener("click", () => {
+    addClassToElement(document.querySelector(".create"));
+    removeClassToElement(document.querySelector(".join"));
+    removeClassToElement(document.querySelector(".createTabBtn"), "select");
+    addClassToElement(document.querySelector(".joinTabBtn"), "select");
+  });
+}
+
+function createGame() {
+  let maxNumber = 3;
+
+  document.querySelector(".setOrderNumberMinus").addEventListener("click", () => {
+    if (maxNumber > 3) {
+      maxNumber -= 1;
+      document.querySelector(".setOrderNumberNow").textContent = maxNumber;
+    }
+  });
+  document.querySelector(".setOrderNumberPlus").addEventListener("click", () => {
+    if (maxNumber < 9) {
+      maxNumber += 1;
+      document.querySelector(".setOrderNumberNow").textContent = maxNumber;
+    }
+  });
+
+  document.querySelector(".createBtn").addEventListener("click", () => {
+    const yourOrder = 1;
+    const numbers = [];
+    for (let i = 0; i < maxNumber; i++) {
+      numbers.push(arrayShuffle(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]));
+    }
+    addThisID(encrypt(numbers));
+
+    addClassToElement(document.querySelector(".create"));
+    removeClassToElement(document.querySelector(".main"));
+    addClassToElement(document.querySelector(".selectCreateOrJoin"));
+    removeClassToElement(document.querySelector(".thisIDtab"));
+    paintCard(numbers);
+  });
+}
+
+function joinGame() {
+  let order = -1;
+  let numbers = [];
+  let error = true;
+  const orderBtns = document.getElementsByClassName("orderBtn");
+
+  document.querySelector(".joinInput").addEventListener("input", () => {
+    try {
+      decrypt(document.querySelector(".joinInput").value);
+      addClassToElement(document.querySelector(".whatJoinID"));
+      error = false;
+    } catch {
+      document.querySelector(".whatJoinID").textContent = "> 참가하시려는 게임의 id를 입력해주세요. 잘못된 코드입니다.";
+      removeClassToElement(document.querySelector(".whatJoinID"));
+      numbers = [];
+      error = true;
+    }
+  });
+
+  for (let i = 0; i < orderBtns.length; i++) {
+    orderBtns.item(i).addEventListener("click", e => {
+      for (let j = 0; j < orderBtns.length; j++) {
+        removeClassToElement(orderBtns.item(j), "select");
+      }
+      addClassToElement(e.target, "select");
+      order = e.target.value;
+      addClassToElement(document.querySelector(".whatJoinOrder"));
     });
   }
 
-  function tabCreateOrJoin() {
-    document.querySelector(".createTabBtn").addEventListener("click", () => {
-      removeClassToElement(document.querySelector(".create"));
+  document.querySelector(".joinBtn").addEventListener("click", () => {
+    if (!error && order != -1) {
+      addThisID(document.querySelector(".joinInput").value);
+      paintCard(decrypt(document.querySelector(".joinInput").value), order);
+
       addClassToElement(document.querySelector(".join"));
-      addClassToElement(document.querySelector(".createTabBtn"), "select");
-      removeClassToElement(document.querySelector(".joinTabBtn"), "select");
-    });
-    document.querySelector(".joinTabBtn").addEventListener("click", () => {
-      addClassToElement(document.querySelector(".create"));
-      removeClassToElement(document.querySelector(".join"));
-      removeClassToElement(document.querySelector(".createTabBtn"), "select");
-      addClassToElement(document.querySelector(".joinTabBtn"), "select");
-    });
-  }
+      removeClassToElement(document.querySelector(".main"));
+      addClassToElement(document.querySelector(".selectCreateOrJoin"));
+      removeClassToElement(document.querySelector(".thisIDtab"));
+    }
+  });
+}
 
-  function setNumber() {
-    document.querySelector(".setNumberMinus").addEventListener("click", () => {
-      if (maxNumber > 3) {
-        maxNumber -= 1;
-        document.querySelector(".setNumberNow").textContent = maxNumber;
-      }
-    });
-    document.querySelector(".setNumberPlus").addEventListener("click", () => {
-      if (maxNumber < 10) {
-        maxNumber += 1;
-        document.querySelector(".setNumberNow").textContent = maxNumber;
-      }
-    });
-  }
+function addThisID(value) {
+  document.querySelector(".thisID").value = value;
+  document.querySelector(".thisID").setAttribute("data-clipboard-text", value);
+}
+
+function paintCard(arrs, order = 1) {
+  document.querySelector(".card").textContent = arrs[order - 1][0];
+}
+
+function footer() {
+  const clipboard = new ClipboardJS(".thisID");
 }
 
 function encrypt(arr) {
